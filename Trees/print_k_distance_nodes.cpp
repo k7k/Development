bd0b195bebb043c5689852bc86c484122c0c62a6
Kavitha K <kavi.chitte@gmail.com>
2017-05-30 22:04:52 +0530

    added new file to print k-distance from node

---
+++ Trees/print_k_distance_nodes.cpp
@@ -0,0 +1,68 @@
+#include "Tree.h"
+
+void printKdown(Tree* node,int k)
+{
+	if(node == NULL || k<0)
+		return;
+
+	if(k == 0)
+	{
+		cout<<node->data<<endl;
+		return;
+	}
+
+	printKdown(node->left,k-1);
+	printKdown(node->right,k-1);
+}
+
+int printKnodes(Tree* root,int target,int dist)
+{
+	if(root == NULL)
+		return -1;
+
+	if(root->data == target)
+	{
+		printKdown(root,dist);
+		return 0;
+	}
+
+	int d1 = printKnodes(root->left,target,dist);
+	if(d1 != -1)
+	{
+		if(d1+1 == dist)
+			cout<<root->data<<endl;
+		else
+			printKdown(root->right,dist-d1-2);
+
+		return d1+1;
+	}
+
+	int d2 = printKnodes(root->right,target,dist);
+	if(d2 != -1)
+	{
+		if(d2+1 == dist)
+			cout<<root->data<<endl;
+		else
+			printKdown(root->left,dist-d2-2);
+		return d2+1;
+	}
+	return -1;
+}
+
+int main()
+{
+	Tree *root = getNode(10);
+	root->left = getNode(7);
+	root->right = getNode(15);
+	root->left->left = getNode(5);
+	root->left->right = getNode(9);
+	root->left->left->left = getNode(3);
+	root->left->left->right = getNode(6);
+	root->right->left = getNode(14);
+	root->right->right = getNode(20);
+
+	int dist = 2;
+	printKnodes(root,15,2);
+
+	return 0;
+}
