6fce584938eaf8bafd74d3b7329d4d99c2b18a36
k7k <kavitha_vrao@ymail.com>
2022-05-19 18:41:25 +0530

    Update destructor.cpp
    some prints

--- general/destructor.cpp
+++ general/destructor.cpp
@@ -10,13 +10,16 @@ public:
 }; 
 void fun(Test t) 
 { 
-    Test(); 
+    Test();
+    cout<<"check 3\n";
     t.~Test(); 
 } 
 int main() 
 { 
     Test(); 
+    cout<<"check 1\n";
     Test t; 
+    cout<<"check 2\n";
     fun(t); 
 	cout<<"funnnnn....\n";
     fun(t); 
